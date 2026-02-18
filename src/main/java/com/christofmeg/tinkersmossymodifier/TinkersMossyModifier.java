ea0f4c8929c93f71698f19d02012f6315be55a63
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2026-02-18 07:10:42 +0100

    wip config

--- src/main/java/com/christofmeg/tinkersmossymodifier/TinkersMossyModifier.java
+++ src/main/java/com/christofmeg/tinkersmossymodifier/TinkersMossyModifier.java
@@ -1,9 +1,11 @@
 package com.christofmeg.tinkersmossymodifier;
 
 import com.christofmeg.tinkersmossymodifier.common.ModRegistries;
+import com.christofmeg.tinkersmossymodifier.common.config.Config;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @SuppressWarnings("removal")
@@ -17,15 +19,13 @@ public class TinkersMossyModifier {
         return new ResourceLocation(MOD_ID, name);
     }
 
-    public TinkersMossyModifier() {
+    public TinkersMossyModifier(FMLJavaModLoadingContext context) {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModRegistries.init(bus);
+
+        context.registerConfig(ModConfig.Type.COMMON, Config.COMMON_CONFIG);
     }
 
     //TODO continue texture cleanup
-    //TODO make config for sunny/rainy
-    //TODO make config for how often items repair
-    //TODO make config for how much items repair each time
-    //TODO make config to also drain EXP
 
 }
---
+++ src/main/java/com/christofmeg/tinkersmossymodifier/common/config/Config.java
@@ -0,0 +1,47 @@
+package com.christofmeg.tinkersmossymodifier.common.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class Config {
+
+    private static final ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
+    public static final String CATEGORY_SETTINGS = "Tinkers Mossy Modifier Mod";
+
+    public static ForgeConfigSpec COMMON_CONFIG;
+    public static ForgeConfigSpec.BooleanValue DOUBLE_REPAIR_RATE_IN_SUN;
+    public static ForgeConfigSpec.BooleanValue DOUBLE_REPAIR_RATE_IN_RAIN;
+    public static ForgeConfigSpec.ConfigValue<Integer> REPAIR_EVERY_X_GAMETICK;
+    public static ForgeConfigSpec.ConfigValue<Integer> BASE_REPAIR_VALUE;
+    public static ForgeConfigSpec.BooleanValue ENABLE_EXPERIENCE_DRAIN;
+    public static ForgeConfigSpec.ConfigValue<Integer> BASE_EXPERIENCE_DRAIN_RATE;
+
+    static {
+        setupFirstBlockConfig();
+        COMMON_CONFIG = COMMON_BUILDER.build();
+    }
+
+    private static void setupFirstBlockConfig() {
+        COMMON_BUILDER.comment("Mod settings").push(CATEGORY_SETTINGS);
+
+        DOUBLE_REPAIR_RATE_IN_SUN = COMMON_BUILDER.comment("Enable double repair value in sun")
+                .define("DoubleRepairInSun", true);
+
+        DOUBLE_REPAIR_RATE_IN_RAIN = COMMON_BUILDER.comment("Enable double repair value in rain")
+                .define("DoubleRepairInRain", false);
+
+        REPAIR_EVERY_X_GAMETICK = COMMON_BUILDER.comment("Repair once x game ticks has passed")
+                .define("RepairEvery", 150);
+
+        BASE_REPAIR_VALUE = COMMON_BUILDER.comment("Default value is 2, formula is (2 + mossyLevel) * (2 if sun/rain is enabled)")
+                .define("BaseRepairValue", 2);
+
+        ENABLE_EXPERIENCE_DRAIN = COMMON_BUILDER.comment("Enable draining of experience to repair, like Mending Moss")
+                .define("EXPDrain", false);
+
+        BASE_EXPERIENCE_DRAIN_RATE = COMMON_BUILDER.comment("If EXPDrain is enabled. How much experience should be drained each repair")
+                .define("BaseEXPDrainValue", 1);
+
+        COMMON_BUILDER.pop();
+    }
+
+}
